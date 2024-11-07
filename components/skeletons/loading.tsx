3d21cbb7297eb3e48506d8140968a5bebeda795c
Darshan Chauhan <46072647+GradleD@users.noreply.github.com>
2024-11-07 08:04:52 +0000

    Updated loading component

--- components/skeletons/loading.tsx
+++ components/skeletons/loading.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Skeleton, CircularProgress } from "@mui/material";
+import { Lock } from "@mui/icons-material"; 
 
 type LoadingType = "skeleton" | "spinner";
 
@@ -19,16 +20,28 @@ const Loading: React.FC<LoadingWrapperProps> = ({
   height = 100,
 }) => {
   if (!isLoading) return <>{children}</>;
-
   switch (loadingType) {
     case "skeleton":
       return (
-        <Skeleton
-          sx={{ bgcolor: "grey.600" }}
-          variant="rectangular"
-          width={width}
-          height={height}
-        />
+        <div style={{ position: 'relative', width, height }}>
+          <Skeleton
+            sx={{ bgcolor: "grey.600" }}
+            variant="rectangular"
+            width="100%"
+            height="100%"
+          />
+          <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <Lock sx={{ marginRight: 1 }} /> {/* Privacy Icon */}
+            <Skeleton variant="text" width={80} />
+          </div>
+        </div>
       );
     case "spinner":
       return (
